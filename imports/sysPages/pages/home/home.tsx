effd84f0a8b7291ab4cb9fc2e3eaf1cda0461dff
jpales1 <joaopedropales@gmail.com>
2025-06-20 14:08:01 -0300

    feat: atualiza a página inicial para exibir mensagem de boas-vindas e organiza as seções, substituindo componentes de notificações e diálogos por atividades recentes.

--- imports/sysPages/pages/home/home.tsx
+++ imports/sysPages/pages/home/home.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import HomeSectionNotificacoes from './sections/notificacoes';
-import HomeSectionDialogs from './sections/dialogs';
+import RecentActivities from './sections/recentActivities';
 import HomeStyles from './homeStyle';
-import HomeSectionComponents from "/imports/sysPages/pages/home/sections/componentTests";
-
 
 const Home: React.FC = () => {
-  const { Container, Header, } = HomeStyles;
+  const { Container, Header } = HomeStyles;
 
 	return (
 		<Container>
       <Header>
-				<Typography variant="h3">Página de testes</Typography>
+				<Typography variant="h3">Bem-vindo ao Sistema de Tarefas</Typography>
 				<Typography variant="body1" textAlign={'justify'}>
-					Bem vindo ao Boilerplate do Synergia. Essa é uma página dedicada aos testes e exibições de componentes e
-					funcionalidades do nosso sistema. Esperamos que você aproveite e aprenda bastante com ela. Para mais dúvidas
-					consulte nossa documentação oficial pelo storybook.
+					Organize suas tarefas de forma eficiente e mantenha-se produtivo! 
+					Aqui você pode acompanhar suas atividades recentes e gerenciar todas as suas tarefas.
 				</Typography>
 			</Header>
-			<HomeSectionNotificacoes />
-			<HomeSectionDialogs />
-      <HomeSectionComponents />
+			<RecentActivities />
 		</Container>
 	);
 };
