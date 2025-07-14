0572d69790af23cd65a0f38c785c147fc9344c11
jpales1 <joaopedropales@gmail.com>
2025-07-14 17:01:41 -0300

    feat: atualiza a página de login com novos textos e links para recuperação de senha e cadastro

--- imports/sysPages/pages/signIn/signIn.tsx
+++ imports/sysPages/pages/signIn/signIn.tsx
@@ -5,18 +5,17 @@ import SysTextField from '../../../ui/components/sysFormFields/sysTextField/sysT
 import SysForm from '../../../ui/components/sysForm/sysForm';
 import SysFormButton from '../../../ui/components/sysFormFields/sysFormButton/sysFormButton';
 import { signInSchema } from './signinsch';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import SysIcon from '../../../ui/components/sysIcon/sysIcon';
 import AuthContext, { IAuthContext } from '/imports/app/authProvider/authContext';
 import AppLayoutContext from '/imports/app/appLayoutProvider/appLayoutContext';
+import Box from '@mui/material/Box';
 
 const SignInPage: React.FC = () => {
 	const { showNotification } = useContext(AppLayoutContext);
 	const { user, signIn } = useContext<IAuthContext>(AuthContext);
 	const navigate = useNavigate();
-	const { Container, Content, FormContainer, FormWrapper } = SignInStyles;
+	const { Container, Content, FormContainer, FormWrapper, RegisterRecovery } = SignInStyles;
 
 	const handleSubmit = ({ email, password }: { email: string; password: string }) => {
 		signIn(email, password, (err) => {
@@ -24,12 +23,13 @@ const SignInPage: React.FC = () => {
 			showNotification({
 				type: 'error',
 				title: 'Erro ao tentar logar',
-				message: 'Email ou senha inválidos',
+				message: 'Email ou senha inválidos'
 			});
 		});
-;	};
+	};
 
 	const handleForgotPassword = () => navigate('/password-recovery');
+	const handleRegister = () => navigate('/signup');
 
 	useEffect(() => {
 		if (user) navigate('/');
@@ -38,34 +38,44 @@ const SignInPage: React.FC = () => {
 	return (
 		<Container>
 			<Content>
-				<Typography variant="h1" display={'inline-flex'} gap={1}>
-					<Typography variant="inherit" color={(theme) => theme.palette.sysText?.tertiary}>
-						{'{'}
-					</Typography>
-					Boilerplate
-					<Typography variant="inherit" color="sysText.tertiary">
-						{'}'}
-					</Typography>
+				<Typography variant="h1" color={'black'}>
+					ToDo List
+				</Typography>
+
+				<Typography variant="body1" color={'black'} textAlign={'center'}>
+					Boas-vindas a sua lista de tarefas.
+					<br />
+					Insira seu e-mail e senha para efetuar o login:
 				</Typography>
 
 				<FormContainer>
-					<Typography variant="h5">Acesse o sistema</Typography>
 					<SysForm schema={signInSchema} onSubmit={handleSubmit} debugAlerts={false}>
 						<FormWrapper>
 							<SysTextField name="email" label="Email" fullWidth placeholder="Digite seu email" />
 							<SysTextField label="Senha" fullWidth name="password" placeholder="Digite sua senha" type="password" />
-							<Button variant="text" sx={{ alignSelf: 'flex-end' }} onClick={handleForgotPassword}>
-								<Typography variant="link">Esqueci minha senha</Typography>
-							</Button>
-							<Box />
+
 							<SysFormButton variant="contained" color="primary" endIcon={<SysIcon name={'arrowForward'} />}>
 								Entrar
 							</SysFormButton>
+
+							<RegisterRecovery>
+								<Typography variant="body1" color={'black'} sx={{ display: 'flex', flexDirection: 'row', gap: 1 }}>
+									Esqueceu sua senha?
+									<Typography variant="link" onClick={handleForgotPassword} sx={{ cursor: 'pointer' }}>
+										Clique aqui
+									</Typography>
+								</Typography>
+
+								<Typography variant="body1" color={'black'} sx={{ display: 'flex', flexDirection: 'row', gap: 1 }}>
+									Novo por aqui?
+									<Typography variant="link" onClick={handleRegister} sx={{ cursor: 'pointer' }}>
+										Cadastre-se
+									</Typography>
+								</Typography>
+							</RegisterRecovery>
 						</FormWrapper>
 					</SysForm>
 				</FormContainer>
-
-				<Box component="img" src="/images/wireframe/synergia-logo.svg" sx={{ width: '100%', maxWidth: '400px' }} />
 			</Content>
 		</Container>
 	);
