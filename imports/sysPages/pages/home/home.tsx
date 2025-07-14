70a8862a32c5f9bf2402a587ab744afca2346384
jpales1 <joaopedropales@gmail.com>
2025-07-14 17:02:11 -0300

    feat: atualiza a página inicial para exibir saudação personalizada e melhorar a descrição das tarefas

--- imports/sysPages/pages/home/home.tsx
+++ imports/sysPages/pages/home/home.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Typography from '@mui/material/Typography';
 import RecentActivities from './sections/recentActivities';
 import HomeStyles from './homeStyle';
+import AuthContext, { IAuthContext } from '/imports/app/authProvider/authContext';
 
 const Home: React.FC = () => {
   const { Container, Header } = HomeStyles;
-
+	const { user } = useContext<IAuthContext>(AuthContext);
 	return (
 		<Container>
       <Header>
-				<Typography variant="h3">Bem-vindo ao Sistema de Tarefas</Typography>
+				<Typography variant="h2">Olá, {user?.username}</Typography>
 				<Typography variant="body1" textAlign={'justify'}>
-					Organize suas tarefas de forma eficiente e mantenha-se produtivo! 
-					Aqui você pode acompanhar suas atividades recentes e gerenciar todas as suas tarefas.
+					Seus projetos muito mais organizados. Veja as tarefas adicionadas por seu time, por você e para você!
 				</Typography>
 			</Header>
 			<RecentActivities />
